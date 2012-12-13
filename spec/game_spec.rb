33e4e61a20c39877688bded19bd589d60c4ce82d
Moss Collum <moss.collum@gmail.com>
2012-12-13 15:17:01 -0500

    Moved robot in example.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,19 +5,17 @@ require 'game'
 describe Game do
   let (:windly) { FakeWindowManager.new }
   subject { Game.new(windly) }
+  before { subject.place_robot_at 6, 14 }
 
   context "when the game is started with the robot on square 2, 14" do
-    before do
-      subject.place_robot_at 2, 14
-      subject.start
-    end
+    before { subject.start }
 
     it "shows a welcome message" do
       windly.row(24).should start_with("Find kitten!")
     end
 
     it "shows the robot as #" do
-      windly.char_at(2, 14).should == '#'
+      windly.char_at(6, 14).should == '#'
     end
   end
 end
