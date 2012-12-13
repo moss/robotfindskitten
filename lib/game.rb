a0d04832ef7a3782472f243c9549956f3408bf17
Moss Collum <moss.collum@gmail.com>
2012-12-13 14:20:59 -0500

    Made Game object responsible for drawing robot.

--- lib/game.rb
+++ lib/game.rb
@@ -3,7 +3,12 @@ class Game
     @window_manager = window_manager
   end
 
+  def place_robot_at row, column
+    @robot_position = [row, column]
+  end
+
   def start
+    @window_manager.print @robot_position[0], @robot_position[1], "#"
     @window_manager.print 24, 0, "Find kitten!"
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,11 +6,18 @@ describe Game do
   let (:windly) { FakeWindowManager.new }
   subject { Game.new(windly) }
 
-  context "when the game is started" do
-    before { subject.start }
+  context "when the game is started with the robot on square 2, 14" do
+    before do
+      subject.place_robot_at 2, 14
+      subject.start
+    end
 
     it "shows a welcome message" do
       windly.row(24).should start_with("Find kitten!")
+    end
+
+    it "shows the robot as #" do
+      windly.char_at(2, 14).should == '#'
     end
   end
 end
